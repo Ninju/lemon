e6d9fe64830241ed75c4e3c374b884f502cdbf9e
Alex Watt <alex.watt@me.com>
2010-04-07 18:39:01 +0100

    Added hello-world.clj (just to test things are working correctly)

---
+++ src/hello-world.clj
@@ -0,0 +1,6 @@
+(ns hello-world.servlet
+  (:gen-class :extends javax.servlet.http.HttpServlet))
+
+(defn -doGet [_ request response]
+  (let [w (.getWriter response)]
+    (.println w "Hello, world!")))
