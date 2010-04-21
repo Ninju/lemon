ee3ab41387e363b45dc2ac18256773aadc2c7332
Nicholas Wright <nick.loves.rails@gmail.com>
2010-04-21 13:12:32 -0600

    start mocking out some of the actions

---
+++ server/src/lime/utils.clj
@@ -0,0 +1,14 @@
+(ns lime.utils)
+
+(defn apply-str
+  "takes a list of arguments and creates a string from them"
+  [& xs]
+  (apply str xs))
+
+(defn split
+  [str delim]
+  (seq (.split str delim)))
+
+(defn nl-to-br [str]
+  (interpose [:br]
+    (split str "\n")))
